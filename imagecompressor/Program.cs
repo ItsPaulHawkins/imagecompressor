7e361e7553a1a2917836dc890e00f83e43bd3731
Paul Hawkins <hawkinsanthonypaul@gmail.com>
2017-02-12 20:28:43 -0500

    Converted image to base64 and wrote the string to a file.

--- imagecompressor/Program.cs
+++ imagecompressor/Program.cs
@@ -11,6 +11,15 @@ namespace imagecompressor
         static void Main(string[] args)
         {
             Console.WriteLine("Please upload an image, and we'll compress it for you!");
+            Console.WriteLine("Type the path of the image you want to compress");
+            string path = Console.ReadLine();
+            byte[] imageArray = System.IO.File.ReadAllBytes(@path);
+            string image = Convert.ToBase64String(imageArray);
+            System.IO.File.WriteAllText(@"C:/Users/PaulPc/desktop/file.txt", image);
+            Console.WriteLine("Done!");
+            Console.ReadLine();
         }
     }
+
 }
+
