d57cc145a788d737959cf71bfbf6eecb4757155b
Paul Hawkins <hawkinsanthonypaul@gmail.com>
2017-02-12 19:48:24 -0500

    Added project files

---
+++ imagecompressor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imagecompressor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please upload an image, and we'll compress it for you!");
+        }
+    }
+}
