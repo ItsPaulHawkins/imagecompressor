b1ef1ebb25132a21c34293203656297bf29dce4e
Paul Hawkins <hawkinsanthonypaul@gmail.com>
2017-02-18 15:51:15 -0500

    Removed original algorithm

--- imagecompressor/Program.cs
+++ imagecompressor/Program.cs
@@ -12,61 +12,14 @@ namespace imagecompressor
         {
             Console.WriteLine("Would you like to compress or decompress a file?");
             string instruction = Console.ReadLine();
-            if (instruction == "compress" || instruction == "Compress")
-            {
-                Console.WriteLine("Please upload an image, and we'll compress it for you!");
-                Console.WriteLine("Type the path of the image you want to compress");
+            if(instruction == "compress" || instruction == "Compress"){
+                Console.WriteLine("Type the path of the uncompressed image.");
                 string path = Console.ReadLine();
-                Console.WriteLine("Type the export path");
-                string export = Console.ReadLine();
                 byte[] imageArray = System.IO.File.ReadAllBytes(@path);
                 string image = Convert.ToBase64String(imageArray);
-                int length = image.Length;
-                Console.WriteLine(length);
-                Console.ReadLine();
-                int count = 0;
-                int secondnumber = 1;
-                int thirdnumber = 2;
-                string compressed = "";
-                while (thirdnumber != length)
-                {
-                    if (image[count] == image[secondnumber] && image[count] == image[thirdnumber])
-                    {
-                        compressed += image[count] + "&";
-                        if (count != length)
-                        {
-                            count += 3;
-                        }
-                        if (secondnumber != length)
-                        {
-                            secondnumber += 2;
-                        }
-                        if (thirdnumber != length)
-                        {
-                            thirdnumber += 3;
-                        }
-
-                    }
-                    else
-                    {
-                        compressed += image[count];
-                        if (count != length)
-                        {
-                            count++;
-                        }
-                        if (secondnumber != length)
-                        {
-                            secondnumber++;
-                        }
-                        if (thirdnumber != length)
-                        {
-                            thirdnumber++;
-
-                        }
-
-                    }
-                }
-                System.IO.File.WriteAllText(@export, compressed);
+                string compresseddouble;
+                string compressedtripple;
+                string compressedquadruple; //randomly check for quadruple redundencys
 
             }
             else
